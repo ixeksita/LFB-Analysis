65ced581912260b2cfc15239a23db6f7795d37e2
Tania Sanchez <tania.sanchezmonroy@gmail.com>
2016-05-03 17:08:36 +0100

    prediction+heatmaps
    Added a Naive Bayes model for the fire incidences based only on the location.
    Added contour maps for the top10 fire incidences and the predicted incidences

--- LFB.R
+++ LFB.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(ggmap)
 library(ggplot2)
 library(readr)
+library(e1071)
 fire<- read.csv("LFB.csv")
 London <- get_map(location=c(-1.25, 51.13, 0.849, 51.76),  color = "bw", maptype = "roadmap", zoom=10)
 
@@ -9,6 +10,7 @@ London <- get_map(location=c(-1.25, 51.13, 0.849, 51.76),  color = "bw", maptype
 fire$DateOfCall<-as.Date(fire$DateOfCall, format="%d-%b-%y")
 fire["Year"]<-as.numeric(format(fire$DateOfCall,"%y"))
 fire$Year<-fire$Year+2000
+fire["Day"]<-as.numeric(format(fire$DateOfCall,"%d"))
 
 #Loading the coordinates 
 coord<-read.csv("BNGandLatLon.csv")
@@ -39,37 +41,38 @@ p<-ggmap(London)+
         axis.title.y=element_blank())
 ggsave("London_top_crimes.png", p, width=14, height=10, units='in')
 
+count<-0
+label<-'.png'
 #contour plots
-contour<-stat_density2d(aes(x=Lon, y=Lat, fill=..level..,alpha=..level..)
-                        ,size=0.1,data=filter(fire,PropertyType=='Car '),
-                        geom='polygon')
+for(i in unique(top10$PropertyType)){
+  gsub("/","-",i, fixed=TRUE) 
+  contour<-stat_density2d(aes(x=Lon, y=Lat, fill=..level..,alpha=..level..)
+  ,size=0.1,data=filter(fire,PropertyType==i),
+  geom='polygon')
 
-cmap<-ggmap(London, extent='device', legend="bottomleft") + contour +
-  scale_alpha_continuous(range=c(0.3,0.55), guide='none') +
-  scale_fill_continuous(low='light blue', high='dark magenta')
-ggsave("London_car.png", cmap, width=14, height=10, units='in')
+  cmap<-ggmap(London, extent='device', legend="bottomleft") + contour +
+    scale_alpha_continuous(range=c(0.3,0.55), guide='none') +
+    scale_fill_continuous(low='light blue', high='dark magenta')+
+    ggtitle(paste(alpha="Heatmap for fire incidences of type:",i))
+  count<-count+1
+  Filename<-paste(count,label, collapse="-")
+  ggsave(Filename, cmap, width=14, height=10, units='in')
+}
 
-#########note add for loop on the property types (unique(top10))
-#### then create a heatmap for each
-#### divide hthe fire in two and run test then final model 
 
 #prepare the data set, the data corresponding to 2012 will be used for training purposes and the rest 
 #for the actual prediction
-train<-fire[fire$Year=="2012",c("PropertyType", "Lat", "Lon")]
-years=c(2012,2013,2014,2015)
-test<-fire[fire$Year %in% years,c("PropertyType", "Lat", "Lon")]
+train_days<-c(1:7, 15:21)
+test_days<-c(8:14, 22:31)
 
-nb_model<-naiveBayes(PropertyType~., data=train)
+train<-fire[fire$Day %in% train_days, c("PropertyType", "Lat", "Lon")]
+test<-fire[fire$Day %in% test_days, c("PropertyType", "Lat", "Lon")]
 
-######Creating the final model
-#This is again assuming that a fire is likely to ocurr only in locations
-#where a fire has been previously observed
-
-finalmodel<- naiveBayes(PropertyType~., data=test)
-predictF<-predict(finalmodel, test)
+#creating the model
+nb_model<-naiveBayes(PropertyType~., data=train, laplace=3)
+predictF<- predict(nb_model, test )
 vec<- as.character(predictF)
 dfP<-data.frame(vec)
-
 
 #adding the prediction to the complete set
 test["PropertyType_pred"]<-dfP
@@ -82,17 +85,20 @@ pcounts<- pcounts[order(-pcounts$Counts),]
 top10p<-test[test$PropertyType_pred %in% pcounts$PropertyType_pred[c(1:10,1)],]
 
 #Plotting the prediction 
-predp<-ggmap(London)+
-  geom_point(data=top10p, aes(x=Lon, y=Lat, color=factor(PropertyType_pred)), alpha=0.10) +
-  guides(colour = guide_legend(override.aes = list(alpha=1.0, size=2.3),
-                               title="Predicted Fire: Property Type")) +
-  scale_colour_brewer(type="qual",palette="Set3") + 
-  ggtitle("Type of property type fire per area based on \n
-          previously observed fire areas") +
-  theme_light(base_size=20) +
-  theme(axis.line=element_blank(),
-        axis.text.x=element_blank(),
-        axis.text.y=element_blank(),
-        axis.ticks=element_blank(),
-        axis.title.x=element_blank(),
-        axis.title.y=element_blank())
+count<-0
+label<-'pred.png'
+#contour plots
+for(i in unique(top10p$PropertyType_pred)){
+  gsub("/","-",i, fixed=TRUE) 
+  contour<-stat_density2d(aes(x=Lon, y=Lat, fill=..level..,alpha=..level..)
+                          ,size=0.1,data=filter(top10p,PropertyType_pred==i),
+                          geom='polygon')
+  
+  cmap<-ggmap(London, extent='device', legend="bottomleft") + contour +
+    scale_alpha_continuous(range=c(0.3,0.55), guide='none') +
+    scale_fill_continuous(low='light blue', high='dark magenta')+
+    ggtitle(paste(alpha="Heatmap for predicted fire incidences of type:",i))
+  count<-count+1
+  Filename<-paste(count,label, collapse="-")
+  ggsave(Filename, cmap, width=14, height=10, units='in')
+}
